21cefbc725ae6e2cd70ccff92d8220987d2bec2b
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-03-21 09:49:41 +0530

    Create chart.py

---
+++ chart.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x  = np.array([1,2,3,4])
+y = x*2
+plt.plot(x,y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Any suitable title")
+plt.show()
